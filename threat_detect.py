60fa054b495bcf3592c657ba63f7b61dc41d0feb
Aqdas Ansari <113194530+Aqdas101@users.noreply.github.com>
2024-07-26 22:40:33 +0500

    Create threat_detect.py

---
+++ threat_detect.py
@@ -0,0 +1,37 @@
+import cv2
+from ultralytics import YOLO
+
+def detect_objects_in_video(video_path):
+    yolo_model = YOLO('best.pt')
+    video_capture = cv2.VideoCapture(video_path)
+    width = int(video_capture.get(3))
+    height = int(video_capture.get(4))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    result_video_path = "detected_objects_video2.avi"
+    out = cv2.VideoWriter(result_video_path, fourcc, 20.0, (width, height))
+
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+        results = yolo_model(frame)
+
+        for result in results:
+            classes = result.names
+            cls = result.boxes.cls
+            conf = result.boxes.conf
+            detections = result.boxes.xyxy
+
+            for pos, detection in enumerate(detections):
+                if conf[pos] >= 0.5:
+                    xmin, ymin, xmax, ymax = detection
+                    label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}"
+                    color = (0, int(cls[pos]), 255)
+                    cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 6)
+                    cv2.putText(frame, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, color, 5, cv2.LINE_AA)
+
+        out.write(frame)
+    video_capture.release()
+    out.release()
+
+    return result_video_path
